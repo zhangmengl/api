5008bd21233e0666adcb4831f7ad7cfa1521addf
zhangmengli <2402784619@qq.com>
2020-06-30 12:15:49 +0800

    解密

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -33,4 +33,29 @@ class TestController extends Controller
     public function receivePost(){
         echo "<pre>";print_r($_POST);echo "</pre>";
     }
+    //接口解密数据
+    public function decrypt(){
+        echo "<pre>";print_r($_POST);echo "</pre>";
+
+        $mothod = "AES-256-CBC";   //加密方法
+        $key = "1910api";   //加密密钥
+        $iv = "hellohelloabc123";   //加密初始化向量
+
+
+        $enc_data=$_POST["data"];   //接收加密密文
+        $sign=$_POST["sign"];   //接收签名
+
+        //验签
+        $local_sign=sha1($enc_data,$key);   //签名
+
+        if($sign==$local_sign){
+            echo "验签成功";echo "<br>";
+            //解密
+            $dec_data = openssl_decrypt($enc_data,$mothod,$key,OPENSSL_RAW_DATA,$iv);
+            echo "解密的数据：".$dec_data;echo '<br>';
+        }else{
+            echo "验签失败";
+        }
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,5 @@ Route::prefix('api')->group(function () {
     Route::get('/test/info','Api\TestController@info');//接口测试
     Route::get('/test/receive','Api\TestController@receive');//接收数据
     Route::post('/test/receivePost','Api\TestController@receivePost');//接收数据  post
+    Route::post('/test/decrypt','Api\TestController@decrypt');//接口解密数据
 });
